83842e0016fa3f8a4b2b5c0046f30a0e785e1aa2
Tony Bowden <tony@tmtm.com>
2015-07-11 17:41:23 +0100

    Initial scraper

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,69 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/bin/env ruby
+# encoding: utf-8
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'open-uri'
+require 'cgi'
+require 'json'
+require 'date'
+require 'colorize'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+def json_from(url)
+  JSON.parse(open(url).read, symbolize_names: true)
+end
+
+def date_from(str)
+  return if str.to_s.empty?
+  return str if str[/^(\d{4})$/]
+  Date.parse(str).to_s
+end
+
+def gender_from(str)
+  return 'male' if str == 'H'
+  return 'female' if str == 'F'
+  raise "Unknown gender #{str}"
+end
+
+def scrape_list(url)
+  json = json_from(url)
+  # puts JSON.pretty_generate json
+  json[:deputes].each do |d|
+    mp = d[:depute]
+
+    data = { 
+      id: mp[:id_an],
+      name: mp[:nom],
+      given_name: mp[:prenom],
+      family_name: mp[:nom_de_famille],
+      gender: gender_from(mp[:sexe]),
+      date_of_birth: mp[:date_naissance],
+      area_id: mp[:num_circo],
+      area: mp[:nom_circo],
+      area_department: mp[:num_deptmt],
+      start_date: mp[:mandat_debut],
+      end_date: mp[:mandat_fin],
+      party: mp[:parti_ratt_financier],
+      faction: mp[:groupe_sigle],
+      website: mp[:url_an],
+      identifier_nos_deputes: mp[:id],
+      twitter: mp[:twitter],
+      term: 14,
+      source: mp[:url_nosdeputes_api],
+    }
+    ScraperWiki.save_sqlite([:id, :term], data)
+  end
+end
+
+term = {
+  id: '14',
+  name: 'XIVe législature de la Ve République',
+  start_date: '2012-06-20',
+}
+ScraperWiki.save_sqlite([:id], term, 'terms')
+
+@URL = 'http://www.nosdeputes.fr/deputes/json'
+scrape_list(@URL)
